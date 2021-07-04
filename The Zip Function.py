0d6f18e87d02cdb96f57239ac4dc3104b815dbc2
FluroAsh <orangeash@hotmail.com.au>
2021-07-04 10:56:01 +1000

    Update The Zip Function.py

--- The Zip Function.py
+++ The Zip Function.py
@@ -5,6 +5,9 @@ dogs_names = ["Elphonse", "Dr. Doggy DDS", "Carter", "Ralph"]
 names_and_dogs_names = zip(owners, dogs_names)
 print(names_and_dogs_names, "\n")
 
+# We can also just print it directly like this:
+'print(list(names_and_dogs_names), "\n")'
+
 # Convert into readable list, assign to a tuple 
 converted_list = list(names_and_dogs_names)
 print(converted_list)
