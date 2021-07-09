c900980f502b8f4937870d94326694f2185db62d
Ashley Thompson <orangeash@hotmail.com.au>
2021-07-09 17:36:23 +1000

    Create Loops Review.py

---
+++ Loops Review.py
@@ -0,0 +1,15 @@
+# Your code below:
+single_digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+squares = []
+cubes = []
+
+# Print for every number in single_digits
+for num in single_digits:
+  # Base number
+  print(num)
+  # Squared
+  squares.append(num**2)
+  print(squares)
+  # Cubed
+  cubes.append(num**3)
+  print(cubes)
