ddd521b7580dba5798460fce66fdddbff135545a
Ashley Thompson <orangeash@hotmail.com.au>
2021-07-28 13:27:35 +1000

    Create Introduction to Functions: Built-in Functions.py

---
+++ Python3/Introduction to Functions: Built-in Functions.py
@@ -0,0 +1,16 @@
+tshirt_price = 9.75
+shorts_price = 15.50
+mug_price = 5.99
+poster_price = 2.00
+
+# Find the max price of our price values
+max_price = max(tshirt_price, shorts_price, mug_price, poster_price)
+print(max_price)
+
+# Find the minimum price of our price values
+min_price = min(tshirt_price, shorts_price, mug_price, poster_price)
+print(min_price)
+
+# Round the price of tshirt_price to 1 decimal place
+rounded_price = round(tshirt_price, 1)
+print(rounded_price)
