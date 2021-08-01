4f6aa9d02d29d93f41d06e348e8cdd299e5a1675
Ashley Thompson <orangeash@hotmail.com.au>
2021-08-01 12:53:42 +1000

    Create Introduction to Functions: Multiple Returns.py

---
+++ Python3/Introduction to Functions: Multiple Returns.py
@@ -0,0 +1,14 @@
+# Function to declare first, second & third most popular locations
+def top_tourist_locations_italy():
+  first = "Rome"
+  second = "Venice"
+  third = "Florence"
+  return first, second, third
+
+# Get the values from our top_tourist_locations_italy function
+most_popular1, most_popular2, most_popular3 = top_tourist_locations_italy()
+
+# Print the values (first, second & third)
+print(most_popular1)
+print(most_popular2)
+print(most_popular3)
