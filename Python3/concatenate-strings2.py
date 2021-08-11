9a80f3405131e36b46f1d7ab0395ae33c0adc176
Ashley Thompson <orangeash@hotmail.com.au>
2021-08-12 09:10:37 +1000

    Update concatenate-strings2.py

--- Python3/concatenate-strings2.py
+++ Python3/concatenate-strings2.py
@@ -4,6 +4,7 @@ last_name = "Matsuki"
 # Using a len() statement as the starting index and omitting the final index lets you slice 'n' characters 
 # from the end of a string, where 'n' is the amount you subtract from len().
 def password_generator(first_name, last_name):
-  return (first_name[-3:] + last_name[-3:])
+  return first_name[len(first_name)-3:] + last_name[len(last_name)-3:]
 
 temp_password = password_generator(first_name, last_name)
+print(temp_password)
