4b25ed888954a466ef7142e2e8d84775feb6f422
Ashley Thompson <orangeash@hotmail.com.au>
2021-08-11 23:35:16 +1000

    Create slicing-strings.py

---
+++ Python3/slicing-strings.py
@@ -0,0 +1,10 @@
+first_name = "Rodrigo"
+last_name = "Villanueva"
+
+# Think 'starts from' 0 'ends before' 5... 
+new_account = last_name[0:5]
+print(new_account)
+
+# Same thing here, 'starts at' INDEX 2, 'ends before' 6... 6-2 = 4! :)
+temp_password = last_name[2:6]
+print(temp_password)
