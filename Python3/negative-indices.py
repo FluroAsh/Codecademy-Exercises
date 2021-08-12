8a5fb5e6ed079d97f5f8f71746489d4cc908dd47
Ashley Thompson <orangeash@hotmail.com.au>
2021-08-12 19:02:52 +1000

    Create negative-indices.py

---
+++ Python3/negative-indices.py
@@ -0,0 +1,8 @@
+company_motto = "Copeland's Corporate Company helps you capably cope with the constant cacophony of daily life"
+
+# We are able to slice the last two characters of our motto by having a starting index of -2 and omitting a final index.
+second_to_last = company_motto[-2:]
+print(second_to_last)
+
+final_word = company_motto[-4:]
+print(final_word)
