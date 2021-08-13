20c8d4b6fe6d19792401a13be7bec5936e7faa4e
Ashley Thompson <orangeash@hotmail.com.au>
2021-08-13 10:55:59 +1000

    Update strings-and-conditionals-II.py

--- Codecademy-CS101/strings-and-conditionals-II.py
+++ Codecademy-CS101/strings-and-conditionals-II.py
@@ -1,10 +1,11 @@
+# Return true if little strings is in big string, otherwise return false
 def contains(big_string, little_string):
   return little_string in big_string
 
 contains("watermelon", "melon")
 contains("watermelon", "berry")
 
-# If character in string one is in string two, and char isn't already in common list, then append the character to common and return our common list at the end of the function. 
+# If character in string one is in string two, and char isn't already in common list, then append the character to common and return our common list at the end of the function
 def common_letters(string_one, string_two):
   common = []
   for char in string_one:
