a19013a2ce381a8ba7a6bee413706bc1bf9cf1ea
Ashley Thompson <orangeash@hotmail.com.au>
2021-08-13 20:14:11 +1000

    Create formatting-methods.py

---
+++ Codecademy-CS101/formatting-methods.py
@@ -0,0 +1,8 @@
+poem_title = "spring storm"
+poem_author = "William Carlos Williams"
+
+poem_title_fixed = poem_title.title()
+print(poem_title, poem_title_fixed)
+
+poem_author_fixed = poem_author.upper()
+print(poem_author, poem_author_fixed)
