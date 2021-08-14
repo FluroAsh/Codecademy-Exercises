00526c7702cf699b0262cf37df4896758d93a59a
Ashley Thompson <orangeash@hotmail.com.au>
2021-08-14 17:00:24 +1000

    Create .find().py

---
+++ Codecademy-CS101/.find().py
@@ -0,0 +1,4 @@
+god_wills_it_line_one = "The very earth will disown you"
+
+# What we're searching in, followed by the module and what we're searching for... 
+disown_placement = god_wills_it_line_one.find('disown')
