0e9862df0ac04a6f706961401cbfed22c5dea1dc
Ashley Thompson <orangeash@hotmail.com.au>
2021-08-14 14:18:01 +1000

    Create joining-strings-II.py

---
+++ Codecademy-CS101/joining-strings-II.py
@@ -0,0 +1,5 @@
+winter_trees_lines = ['All the complicated details', 'of the attiring and', 'the disattiring are completed!', 'A liquid moon', 'moves gently among', 'the long branches.', 'Thus having prepared their buds', 'against a sure winter', 'the wise trees', 'stand sleeping in the cold.']
+
+# Creates a new line ('\n') at every index in our list [0, 1, 2, 3, 4, 5 etc.]
+winter_trees_full = '\n'.join(winter_trees_lines)
+print(winter_trees_full)
