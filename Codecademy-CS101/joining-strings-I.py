68eef243dd4ce2e48576520beed4b44190dbd9c3
Ashley Thompson <orangeash@hotmail.com.au>
2021-08-14 13:03:53 +1000

    Create joining-strings-I.py

---
+++ Codecademy-CS101/joining-strings-I.py
@@ -0,0 +1,4 @@
+reapers_line_one_words = ["Black", "reapers", "with", "the", "sound", "of", "steel", "on", "stones"]
+
+# Simliar to split() joins using delimiter with the character ' '
+reapers_line_one = (' '.join(reapers_line_one_words))
