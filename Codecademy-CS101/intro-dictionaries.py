1d2f447371ab4af5c3eb4ac16cfcafe28f893864
Ashley Thompson <orangeash@hotmail.com.au>
2021-08-23 23:53:57 +1000

    Create intro-dictionaries.py

---
+++ Codecademy-CS101/intro-dictionaries.py
@@ -0,0 +1,5 @@
+# Each pair to be seperated by a ','
+sensors =  {"pantry": 22, "living room": 21, "kitchen": 23, "bedroom": 20}
+num_cameras = {"backyard": 6,  "garage": 2, "driveway": 1}
+
+print(sensors)
