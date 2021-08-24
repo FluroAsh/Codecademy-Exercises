f582749ddc7cea60541075dd22d613ef262f06cc
Ashley Thompson <orangeash@hotmail.com.au>
2021-08-24 23:22:57 +1000

    Create dictionaries-overwrite-values.py

---
+++ Codecademy-CS101/dictionaries-overwrite-values.py
@@ -0,0 +1,8 @@
+oscar_winners = {"Best Picture": "La La Land", "Best Actor": "Casey Affleck", "Best Actress": "Emma Stone", "Animated Feature": "Zootopia"}
+
+# Add 'Supporting Actress' key
+oscar_winners['Supporting Actress'] = 'Viola Davis'
+
+# Change key 'Best Picture' to value 'Moonlight'
+oscar_winners['Best Picture'] = 'Moonlight'
+print(oscar_winners)
