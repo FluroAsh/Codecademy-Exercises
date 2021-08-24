bd18ae33aa19505bcfd31f92ea35ce0eea8d5b3b
Ashley Thompson <orangeash@hotmail.com.au>
2021-08-24 23:03:45 +1000

    Create dictionaries-add-a-key.py

---
+++ Codecademy-CS101/dictionaries-add-a-key.py
@@ -0,0 +1,8 @@
+# Declare empty list
+animals_in_zoo = {}
+
+# Add new key(s) to our empty dictionary
+animals_in_zoo["zebras"] = 8
+animals_in_zoo["monkeys"] = 12
+animals_in_zoo["dinosaurs"] = 0
+print(animals_in_zoo)
