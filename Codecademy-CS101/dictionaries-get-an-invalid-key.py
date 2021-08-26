287c1f42dd2554df66d8b63da4ba648bfcd82a25
Ashley Thompson <orangeash@hotmail.com.au>
2021-08-26 18:37:42 +1000

    Create dictionaries-get-an-invalid-key.py

---
+++ Codecademy-CS101/dictionaries-get-an-invalid-key.py
@@ -0,0 +1,7 @@
+zodiac_elements = {"water": ["Cancer", "Scorpio", "Pisces"], "fire": ["Aries", "Leo", "Sagittarius"], "earth": ["Taurus", "Virgo", "Capricorn"], "air":["Gemini", "Libra", "Aquarius"]}
+
+# Add new key with value 'Not a Zodiac element' to our dictionary...
+zodiac_elements['energy'] = 'Not a Zodiac element'
+
+# Print!
+print(zodiac_elements["energy"])
