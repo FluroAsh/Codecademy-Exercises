f8a889d64f6dfcb8b30a910902b71e95c5c94245
Ashley Thompson <orangeash@hotmail.com.au>
2021-08-26 18:17:02 +1000

    Create dictionaries-using-a-key.py

---
+++ Codecademy-CS101/dictionaries-using-a-key.py
@@ -0,0 +1,5 @@
+zodiac_elements = {"water": ["Cancer", "Scorpio", "Pisces"], "fire": ["Aries", "Leo", "Sagittarius"], "earth": ["Taurus", "Virgo", "Capricorn"], "air":["Gemini", "Libra", "Aquarius"]}
+
+# Access & print the following values associated with the key(s):
+print(zodiac_elements['earth'])
+print(zodiac_elements['fire'])
