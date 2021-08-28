b5c4d6710495acb7a2e4f71d713098400bff570a
Ashley Thompson <orangeash@hotmail.com.au>
2021-08-28 16:05:22 +1000

    Create dictionaries-get-all-keys.py

---
+++ Codecademy-CS101/dictionaries-get-all-keys.py
@@ -0,0 +1,7 @@
+user_ids = {"teraCoder": 100019, "pythonGuy": 182921, "samTheJavaMaam": 123112, "lyleLoop": 102931, "keysmithKeith": 129384}
+num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
+
+# Assigns all of the keys in 'user_ids' and 'num_exercises' to a new seperate variable
+users = user_ids.keys()
+lessons = num_exercises.keys()
+print(str(users) + '\n\n' + str(lessons))
