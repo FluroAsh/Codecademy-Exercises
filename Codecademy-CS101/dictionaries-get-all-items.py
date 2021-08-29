3f35f37878026d83ece6af3cfda9cdebabbf5484
Ashley Thompson <orangeash@hotmail.com.au>
2021-08-29 18:21:58 +1000

    Update dictionaries-get-all-items.py

--- Codecademy-CS101/dictionaries-get-all-items.py
+++ Codecademy-CS101/dictionaries-get-all-items.py
@@ -3,4 +3,4 @@ pct_women_in_occupation = {"CEO": 28, "Engineering Manager": 9, "Pharmacist": 58
 # To produce our string, use a loop with occuptation (key), value in our dictionary (pct_women_in_occupation) 
 # and use .items to get BOTH the keys and values.
 for occuptation, value in pct_women_in_occupation.items():
-  print('Women make up: ' + str(value) + ' percent of ' + str(occuptation) + 's.')
+  print('Women make up ' + str(value) + ' percent of ' + str(occuptation) + 's.')
