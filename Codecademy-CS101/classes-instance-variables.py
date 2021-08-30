2f4bd32228a0b94d8b39b81e2cb82c54ebdfa4c3
Ashley Thompson <orangeash@hotmail.com.au>
2021-08-30 20:17:08 +1000

    Create classes-instance-variables.py

---
+++ Codecademy-CS101/classes-instance-variables.py
@@ -0,0 +1,10 @@
+class Store:
+  pass
+
+alternative_rocks = Store()
+isabelles_ices = Store()
+
+# Instance variables, NOT class variables
+alternative_rocks.store_name = "Alternative Rocks"
+isabelles_ices.store_name = "Isabelle's Ices"
+print(alternative_rocks)
