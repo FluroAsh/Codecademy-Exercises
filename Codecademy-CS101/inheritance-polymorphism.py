5af29b663add903eec7c98424990b11ba5641ad9
Ashley Thompson <orangeash@hotmail.com.au>
2021-09-01 18:35:46 +1000

    Create inheritance-polymorphism.py

---
+++ Codecademy-CS101/inheritance-polymorphism.py
@@ -0,0 +1,7 @@
+a_list = [1, 18, 32, 12]
+a_dict = {'value': True}
+a_string = "Polymorphism is cool!"
+
+print(len(a_list)) # 4 Integers
+print(len(a_dict)) # 1 Key:Value pair
+print(len(a_string)) # 21 Characters
