b4fad06aeffbbb843706b12b8d4536a2cb1d120b
Ashley Thompson <orangeash@hotmail.com.au>
2021-09-11 22:01:24 +1000

    Create linked-list-implementation-III.py

---
+++ Codecademy-CS101/linked-list-implementation-III.py
@@ -0,0 +1,52 @@
+# We'll be using our Node class
+class Node:
+  def __init__(self, value, next_node=None):
+    self.value = value
+    self.next_node = next_node
+    
+  def get_value(self):
+    return self.value
+  
+  def get_next_node(self):
+    return self.next_node
+  
+  # This method will connect our 2 nodes together, eg. 'a -> c' in the event we remove 'b'
+  def set_next_node(self, next_node):
+    self.next_node = next_node
+
+# Our LinkedList class
+class LinkedList:
+  def __init__(self, value=None):
+    self.head_node = Node(value)
+  
+  def get_head_node(self):
+    return self.head_node
+  
+  def insert_beginning(self, new_value):
+    new_node = Node(new_value)
+    new_node.set_next_node(self.head_node)
+    self.head_node = new_node
+    
+  def stringify_list(self):
+    string_list = ""
+    current_node = self.get_head_node()
+    while current_node:
+      if current_node.get_value() != None:
+        string_list += str(current_node.get_value()) + "\n"
+      current_node = current_node.get_next_node()
+    return string_list
+  
+  # Searches for the node that contains the value to remove it, 
+  # loops through all our nodes until our method finds it or we reach a node with no value ('the tail').
+  def remove_node(self, value_to_remove):
+    current_node = self.get_head_node()
+    if current_node.get_value() == value_to_remove:
+      self.head_node = current_node.get_next_node()
+    else:
+      while current_node:
+        next_node = current_node.get_next_node()
+        if next_node.get_value() == value_to_remove:
+          current_node.set_next_node(next_node.get_next_node())
+          current_node = None # Ends the loop
+        else:
+          current_node = next_node
