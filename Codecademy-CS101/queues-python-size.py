a9bbcb210baa105ee0517fb342296a68f1a8e452
Ashley Thompson <orangeash@hotmail.com.au>
2021-09-20 11:36:41 +1000

    Create queues-python-size.py

---
+++ Codecademy-CS101/queues-python-size.py
@@ -0,0 +1,29 @@
+from node import Node
+# Create the Queue class below:
+class Queue:
+  def __init__(self, max_size=None):
+    self.head = None
+    self.tail = None
+    self.max_size = max_size
+    self.size = 0
+  
+  def peek(self):
+    if self.size > 0:
+      return Node.get_value(self.head)
+    print("Nothing to see here!")
+    
+  def get_size(self):
+    return self.size
+
+  def has_space(self):
+    if self.max_size == None:
+      return True
+    elif self.max_size > self.get_size():
+      return True
+    else:
+      return False
+
+  def is_empty(self):
+    if self.size == 0:
+      return True
+    return False  
