2ec9b28057a150e3778e4cdb18bed8b0c67a30fd
Ashley Thompson <orangeash@hotmail.com.au>
2021-10-08 22:27:10 +1100

    Update creating-the-hash-map-class.py

--- Codecademy-CS101/creating-the-hash-map-class.py
+++ Codecademy-CS101/creating-the-hash-map-class.py
@@ -2,6 +2,6 @@ class HashMap:
   
   def __init__(self, array_size):
     self.array_size = array_size 
-    # List comprehension to create a list as length specified in array_size
-    # Each element set to None
+    ''' List comprehension to create a list as length specified in array_size
+    Each element set to None'''
     self.array = [None for i in range(self.array_size)]
