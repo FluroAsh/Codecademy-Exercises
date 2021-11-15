577a0ede2c246547b469784fa3eed4ed5510d42e
Ashley Thompson <workashleythompson@gmail.com>
2021-11-16 00:02:02 +1100

    Update adding-blocks-to-the-blockchain.py

--- blockchain/adding-blocks-to-the-blockchain.py
+++ blockchain/adding-blocks-to-the-blockchain.py
@@ -1,15 +1,3 @@
-Now that we have everything in place, let’s begin adding blocks to the blockchain.
----
-[Hint: The proper way to access a specific block’s hash from the chain is through this line of code: self.chain[index].hash. 
-- The previous block can be accessed by self.chain[len(self.chain)-1].
-- Your resulting code should be as follows: Block(transactions, previous_block_hash)]
-
--
-1. Complete the function add_block().
-
-To do this, create a variable named new_block that takes in a transaction and the previous_block‘s hash. Append new_block to the end of the chain.
-
-```
 #imports the Block class from block.py
 from block import Block
 class Blockchain:
@@ -34,9 +22,8 @@ class Blockchain:
   
   # add block to blockchain `chain`
   def add_block(self, transactions):
-    # self.chain[len(self.chain))-1] gets the previous_hash dictionary
-    # .hash then retrieves the actual value associated with the key
+    # self.chain[len(self.chain))-1] gets the previous blocks dictionary
+    # .hash then retrieves the actual value associated with hash key
     new_block = Block(transactions,self.chain[len(self.chain)-1].hash)
     self.chain.append(new_block)
     return 
-```
