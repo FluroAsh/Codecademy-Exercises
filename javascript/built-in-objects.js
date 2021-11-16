fa559f0bfefc3d4064da4d98d03bcd0166f2a3eb
Ashley Thompson <workashleythompson@gmail.com>
2021-11-17 00:28:00 +1100

    Create built-in-objects.js

---
+++ javascript/built-in-objects.js
@@ -0,0 +1,6 @@
+// Get number between 0 and 100, round to nearest integer 
+console.log(Math.floor(Math.random() * 100));
+// Returns the smallest integer greater than or equal to '43.8' 
+console.log(Math.ceil(43.8))
+// Checks to see if 2017 is a valid integer, returns true or false
+console.log(Number.isInteger(2017))
